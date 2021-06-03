abf9a31b5d0fc529690d2f3799ed91e755f2736a
Giacomo Bergami <bergamigiacomo@gmail.com>
2021-06-03 19:44:02 +0200

    Algorithm working perfectly on the exact case... Still, need to investigate why it behaves badly on wrong strings that are not recognized.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ template <typename DBL> probabilisitc_model_trace viterbi_top1(const weigthed_la
                                                                 const log_trace& Y) {
     const size_t K = graph.V_size;
     const size_t T = Y.size();
+
     typename TransitionGraph<DBL>::Matrix T1(graph.V_size, Y.size()),
                                         T2(graph.V_size, Y.size());
 
@@ -24,13 +25,15 @@ template <typename DBL> probabilisitc_model_trace viterbi_top1(const weigthed_la
     {
         const std::string& ref = Y.at(0);
         if (node_label(&graph, src) == ref) {
-            T1(src, graph.final_labels.at(ref)) = 1.0;
+            T1(src, 0) = 1.0;
         }
     }
 
     // Mapping each label within the string
     for (size_t j = 1, NN = Y.size(); j < NN; j++) {
         const std::string& alpha = Y.at(j);
+
+        std::cerr << alpha << std::endl;
 
         auto it = graph.label_conversion.find(alpha);
         if (it != graph.label_conversion.end()) {
@@ -53,7 +56,34 @@ template <typename DBL> probabilisitc_model_trace viterbi_top1(const weigthed_la
         }
     }
 
+    std::cout << T1 << std::endl;
+    std::cout << T2 << std::endl;
 
+    DBL    k;
+    size_t karg;
+    for (size_t karg_curr = 0; karg_curr< graph.V_size; karg_curr++) {
+        DBL kcurr = T1(karg_curr, T-1);
+        if ((karg_curr == 0) || (kcurr > k)) {
+            k = kcurr;
+            karg = karg_curr;
+        }
+    }
+
+    probabilisitc_model_trace pmt;
+    auto& ref = pmt.underlying_sequences.emplace_back();
+    ref.sequence.emplace_back(karg);
+    pmt.t_with_prob.t.emplace_back(graph.inv_label_conversion.at(karg));
+
+    for (size_t j = T-1; j>0; j--) {
+        karg = T2(karg, j);
+        ref.sequence.emplace_back(karg);
+        pmt.t_with_prob.t.emplace_back(graph.inv_label_conversion.at(karg));
+    }
+
+    std::reverse(ref.sequence.begin(), ref.sequence.end());
+    std::reverse(pmt.t_with_prob.t.begin(), pmt.t_with_prob.t.end());
+    pmt.t_with_prob.probability = k;
+    return pmt;
 
 #if 0
     size_t i = src;
@@ -103,4 +133,14 @@ template <typename DBL> probabilisitc_model_trace viterbi_top1(const weigthed_la
 }
 
 int main() {
+
+    weigthed_labelled_automata graph;
+    from_string(graph, {"ciao", "mamma", "guarda", "come", "mi", "diverto"});
+    dot(&graph, std::cout);/*
+    std::cout <<
+              viterbi_top1<double>(graph, 0, 5, {"ciao", "mamma", "guarda", "come", "mi", "diverto"}) << std::endl;
+    std::cout <<
+              viterbi_top1<double>(graph, 0, 5, {"ciao", "mamma"}) << std::endl;*/
+    std::cout <<
+              viterbi_top1<double>(graph, 0, 5, {"ciao", "oddio", "mamma", "guarda", "bene"}) << std::endl;
 }
